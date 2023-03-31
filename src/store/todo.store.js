452b7d14b7422139dad8b45fda7e7da7b9a4f9ce
Kevin Ramirez <kev17080753@gmail.com>
2023-03-30 21:52:02 -0600

    Se crean nuevas clases y actualizan segun curso

---
+++ src/store/todo.store.js
@@ -0,0 +1,106 @@
+import { Todo } from "../todos/models/todo.model";
+
+const Filters = {
+    All: 'all',
+    Completed: 'Completed',
+    Pending: 'Pending'
+}
+
+
+
+const state = {
+    todos: [
+        new Todo ('Piedra del alma'),
+        new Todo ('Piedra del mente'),
+        new Todo ('Piedra del tiempo'),
+        new Todo ('Piedra del poder'),
+        new Todo ('Piedra del realidad'),
+        new Todo ('Piedra del espacio'),
+
+
+    ],
+    filter: Filters.All,
+}
+
+const initStore = () => {
+    console.log(state);
+    console.log('InitStore :*');
+}
+
+const loadStore = () => {
+    throw new Error('No implemented');
+}
+
+const getTodos = (filter = Filters.All) => {
+    switch( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done);
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done);
+        default:
+            throw new Error(`Option ${ filter } is not valid!`);
+        
+    }
+}
+
+/**
+ * 
+ * @param {String} descripcion 
+ */
+const addTodo = ( descripcion ) => {
+    if ( !descripcion ) throw new Error('Description is required');
+
+    state.todos.push( new Todo(descripcion) );
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = (todoId) => {
+    state.todos = state.todos.map( todo => {
+        if ( todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = (todoId) => {
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter(todo => todo.done);
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+export default {
+    addTodo,
+    deleteCompleted,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
+}
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,15 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from '../use-cases/render-todo';
+import { Todo } from './models/todo.model';
+
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
+    // DeleteTodo: '.destroy',
+}
+
 /**
  * 
  * @param {String} elementId 
@@ -6,11 +17,48 @@ import html from './app.html?raw';
 export const App = (elementId) => {
 
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        renderTodos(ElementIDs.TodoList, todos);
+    }
+
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
 
+    //Referencias HTML
+    const NewTodoInput = document.querySelector( ElementIDs.NewTodoInput );
+    const todoListUL = document.querySelector( ElementIDs.TodoList );
+    // const todoDeleteTodo = document.querySelector( ElementIDs.DeleteTodo );
+
+    //Listeners
+    NewTodoInput.addEventListener('keyup', (event) => {
+
+        if( event.keyCode !== 13 ) return;
+        if ( event.target.value.trim().length === 0) return;
+
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value = '';
+    }) 
+
+    todoListUL.addEventListener('click', (event) => {
+        const element = event.target.closest('[data-id]');
+        todoStore.toggleTodo( element.getAttribute( 'data-id' ) );
+        displayTodos();
+    })
+
+    todoListUL.addEventListener('click', (event) => {
+        const isDestroyElement = event.target.className === 'destroy';
+        const element = event.target.closest('[data-id]');
+
+        if (!element || !isDestroyElement) return;
+
+        todoStore.deleteTodo( element.getAttribute( 'data-id' ) );
+        displayTodos();
+    })
 
 }
